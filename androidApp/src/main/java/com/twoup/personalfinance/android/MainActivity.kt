542e5ccf69c9f840b635d5abad89f2b328c019fc
Dawn Nguyen <lophocpzo@gmail.com>
2023-06-28 13:50:21 +0700

    Merge setup of ios app from Tam

--- androidApp/src/main/java/com/twoup/personalfinance/android/MainActivity.kt
+++ androidApp/src/main/java/com/twoup/personalfinance/android/MainActivity.kt
@@ -6,8 +6,7 @@ import androidx.activity.compose.setContent
 import com.twoup.personalfinance.ui.MainView
 import org.koin.core.component.KoinComponent
 
-class #MainActivity : ComponentActivity(), KoinComponent {
-
+class MainActivity : ComponentActivity(), KoinComponent {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
