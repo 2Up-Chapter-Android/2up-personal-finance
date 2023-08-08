be3201cc1895603c2eeb0771818f627ec953f418
lenhathuy <huyhuy2899@gmail.com>
2023-08-08 21:50:20 +0700

    [REF] craete UI daily screen

--- features/Main/src/commonMain/kotlin/com/aicontent/main/presentation/daily/DailyScreen.kt
+++ features/Main/src/commonMain/kotlin/com/aicontent/main/presentation/daily/DailyScreen.kt
@@ -1,16 +1,17 @@
 package com.aicontent.main.presentation.daily
 
 
-import androidx.compose.foundation.background
+import PersonalFinance.features.Main.MR
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import com.aicontent.main.presentation.TopAppBar
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Divider
 import androidx.compose.material.FabPosition
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
@@ -20,25 +21,26 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import PersonalFinance.features.Main.MR
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.model.rememberScreenModel
+import cafe.adriel.voyager.core.registry.rememberScreen
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
 import com.aicontent.main.presentation.BudgetBox
-import com.aicontent.main.presentation.TapBarViewModel
-import dev.icerock.moko.resources.compose.localized
-import dev.icerock.moko.resources.desc.desc
-import io.github.aakira.napier.Napier
+import com.aicontent.main.presentation.TopAppBar
+import com.twoup.personalfinance.navigation.TransactionSharedScreen
+import dev.icerock.moko.resources.compose.painterResource
 
 class DailyScreen() : Screen {
     @Composable
     override fun Content() {
+        val navigator = LocalNavigator.currentOrThrow
+        val transactionScreen = rememberScreen(TransactionSharedScreen.CreateTransactionScreen)
         val viewModel = rememberScreenModel { DailyScreenViewModel() }
 
         Scaffold(
@@ -53,18 +55,41 @@ class DailyScreen() : Screen {
             content = {
                 Column{
                     BudgetBox()
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center
+
+                    LazyColumn(
+                        contentPadding = PaddingValues(vertical = 8.dp),
+                        verticalArrangement = Arrangement.spacedBy(16.dp),
                     ) {
-                        Text(MR.strings.daily_screen.desc().localized())
+                        // Add a single item
+                        item {
+                            Text(text = "First item")
+                        }
+
+                        // Add 5 items
+                        items(5) { index ->
+                            ItemTransaction()
+                            Divider(color = Color.Black, thickness = 0.5.dp)
+                        }
+
+                        // Add another single item
+                        item {
+                            Text(text = "Last item")
+                        }
                     }
+//                    Column(
+//                        modifier = Modifier.fillMaxSize(),
+//                        horizontalAlignment = Alignment.CenterHorizontally,
+//                        verticalArrangement = Arrangement.Center
+//                    ) {
+//
+//                        Text(MR.strings.daily_screen.desc().localized())
+//                    }
                 }
             },
             floatingActionButton = {
                 FloatingActionButton(
                     onClick = {
+                        navigator.push(transactionScreen)
                     },
                     backgroundColor = MaterialTheme.colors.primary,
                 ) {
@@ -77,5 +102,27 @@ class DailyScreen() : Screen {
             },
             floatingActionButtonPosition = FabPosition.End
         )
+    }
+
+    @Composable
+    fun ItemTransaction() {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                contentDescription = "My Image",
+                modifier = Modifier
+                    .padding(16.dp)
+                    .width(32.dp)
+                    .height(32.dp),
+                painter = painterResource(MR.images.logo_otp),
+
+            )
+
+            Column(modifier = Modifier.weight(1f)) {
+                Text(modifier = Modifier.padding(vertical = 4.dp) ,text = "First item", color = Color.Black, fontSize = 14.sp)
+                Text(text = "First item", fontSize = 12.sp)
+            }
+
+            Text(modifier = Modifier.padding(end = 8.dp),text = "First item")
+        }
     }
 }
--- features/Main/src/commonMain/kotlin/com/aicontent/main/presentation/daily/DailyScreenViewModel.kt
+++ features/Main/src/commonMain/kotlin/com/aicontent/main/presentation/daily/DailyScreenViewModel.kt
@@ -3,10 +3,20 @@ package com.aicontent.main.presentation.daily
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import cafe.adriel.voyager.core.model.ScreenModel
+import com.twoup.personalfinance.domain.model.wallet.getWallet.GetListWalletResponseModel
+import com.twoup.personalfinance.utils.data.Resource
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import org.koin.core.component.KoinComponent
 
 class DailyScreenViewModel : ScreenModel, KoinComponent {
     
     var selectedTabIndex: MutableState<Int> = mutableStateOf(0)
+
+    private val _createTransUiState = MutableStateFlow(TransUiState())
+    val createTransUiState = _createTransUiState.asStateFlow()
+
+    // insert get list transaction
+
 
 }
